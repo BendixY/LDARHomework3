13529eb66a30a21b91237d5e1ce9fbd323776d01
BendixY <benedict.wuethrich@gmail.com>
2024-11-06 12:31:24 +0100

    got the files

---
+++ Homework3.R
@@ -0,0 +1,28 @@
+# Homework 3 - Linear Regression
+# name(s):
+
+
+# load all packages here:
+
+
+# 1) Load the data: associations.csv and inspect the data frame - how many data points and variables are there? What types of variables are there?
+
+
+
+# 2.a) Explore all the variables in your data set: provide summary statistics (measures of central tendency) and plot each one of them (except WORD). Describe what you see in the plots and the summary statistics. Is there anything striking? Are some of the variables very skewed and need to be transformed? If so, try some of the transformations we learned about and see if that helps. Only use them if they really improve things a lot.
+
+
+# 2.b) Plot the dependent variable (ASSOC) with each of the predictors (all the other variables) and give a brief explanation of what you see.
+
+
+
+# 3.a) Make a linear model with ASSOC as the dependent variable and all other variables as predictors. Start with the maximal model including all interactions and perform manual model selection until you arrive at the final model.
+# For each model, display the summary and briefly explain what it means.
+
+
+# 3.b) Plot the effects of the final model and give a brief summary of what the plot shows.
+
+
+# 3.c) Plot the residuals of the final model. Is there a discernible pattern or not? Are they (more or less) normally distributed?
+
+
